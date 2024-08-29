349cc2fb9eeae39dde8774aed5d73105d161f021
fdoquezada <fdoquezadapuno@gmail.com>
2024-08-28 22:17:43 -0400

    creacion de de tarea clase 5

--- src/index.js
+++ src/index.js
@@ -1,7 +1,4 @@
-const express = require('express')
-const app = express()
-const port = 3000
+import app from './app.js'
 
-app.get('/', (req, res) => res.send('Hello World!'))
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
-
+app.listen(3000)
+console.log('listening on port', 3000)
